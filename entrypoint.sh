30af3fbb9c8bb43c509991c91d67401146098200
Benjamin Texier <benjamin.texier@outlook.com>
2022-02-15 13:10:51 +0100

    fix(entrypoint): exit istio-sidecar after jobs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/sh -
 
 set -e
 
-SCRIPT_DIR="$(dirname $0)"
+script_dir="$(dirname $0)"
+istio_quit_endpoint=${ISTIO_QUIT_ENDPOINT:-"http://localhost:15020/quitquitquit"}
+istio_ready_endpoint=${ISTIO_READY_ENDPOINT:-"http://localhost:15020/healthz/ready"}
 
-ISTIO_QUIT_ENDPOINT=${ISTIO_QUIT_ENDPOINT:-"http://localhost:15020/quitquitquit"}
-ISTIO_READY_ENDPOINT=${ISTIO_READY_ENDPOINT:-"http://localhost:15020/healthz/ready"}
-
-waiting(){
+waiting_for_istio(){
   url=${1}
-  until ${SCRIPT_DIR}/xh ${url} --check-status; do
-    echo waiting for ${url}
+  until ${script_dir}/xh ${istio_ready_endpoint} --check-status; do
+    echo waiting for ${istio_ready_endpoint}
     sleep 1;
   done
 }
 
 quitquitquit(){
-  url=${1}
-  ${SCRIPT_DIR}/xh POST ${url}
+  ${script_dir}/xh POST ${istio_quit_endpoint}
 }
 
-run(){
-  exec $@
-}
-
-waiting ${ISTIO_READY_ENDPOINT}
-quitquitquit ${ISTIO_QUIT_ENDPOINT}
-run $@
+waiting_for_istio
+/bin/sh << EOF
+$@
+EOF
+quitquitquit
